1c7640b7117d8e31365f16a899d51e38ed0289b5
Tony Gaskell <thgaskell@gmail.com>
2014-09-27 15:47:38 -1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# JavaScript Basics
+
+- Run `http-server` from the root directory.
+- Open up your browser to [http://localhost:8080](http://localhost:8080) in your browser.
+
+Only modify your the `script.js` file by following the comments to make all the tests pass.
---
+++ script.js
@@ -0,0 +1,24 @@
+/* Create a `name` variable and assign it a String value */
+
+/* Create a `person` variable and give it 2 properties,
+ * `name`, assign it the same name as before,
+ * as well as an `age` (number);
+ */
+
+/* Create a function called `greet`,
+ * it should take a 1 parameter, `name`
+ * and it should print "Hello, my {name}"
+ */
+
+/* Create a variable called `canDrive`,
+ * if it should be true if your person object is at least 16 years old
+ */
+
+/* Create a variable called `dataTypes` with atleast 1 of every data type;
+ * (there are 6 different data types);
+ */
+
+/* Create a Dog object using the `new` constructor.
+ * it should have a `bark` function that makes your dog bark!
+ * It should also have a name attribute.
+ */
